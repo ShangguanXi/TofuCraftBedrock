0293335dce5ab34e31e601950af3e41fc3af6b78
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2026-03-04 23:44:21 +0800

    feat：烤豆腐

--- tofucraft[B]/typescripts/main.ts
+++ tofucraft[B]/typescripts/main.ts
@@ -1,6 +1,3 @@
-import { EntityEquippableComponent, EquipmentSlot, world, MolangVariableMap, system, ItemUseAfterEvent } from "@minecraft/server";
-import { ItemAPI } from "./lib/ItemAPI";
-import { EventAPI } from "./lib/EventAPI";
 
 // Import custom components to register them
 import "./components/BlockTofuKinuComponent";
